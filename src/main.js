f3f848b421a9f60b9425ef6080b96564a74d3a96
tomikorzu <tomykorzu@icloud.com>
2024-10-09 10:48:44 -0300

    feat(main.js): add showQuote and fetchData functions

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
 import $ from "jquery";
-$("#app").click(function () {
-  alert("Text: ");
-});
+
+const saveQuotes = JSON.parse(localStorage.getItem("quotes")) || [];
+
+const showQuote = (data) => {
+  let quote = data[0].quote;
+  let author = data[0].author;
+
+  if (!saveQuotes.includes(quote)) {
+    saveQuotes.push(quote);
+    localStorage.setItem("quotes", JSON.stringify(saveQuotes));
+  }
+
+  $("#quote").text(quote);
+  $("#author").text(author);
+};
+
+const fetchData = () => {
+  $.getJSON("https://api.breakingbadquotes.xyz/v1/quotes/", (data) => {
+    showQuote(data);
+  });
+};
+
+const deleteQuote = (quote) => {
+  
+}
+
+const runCode = () => {
+  fetchData();
+  $("#quote-btn").click(fetchData);
+};
+
+$(document).ready(runCode);
