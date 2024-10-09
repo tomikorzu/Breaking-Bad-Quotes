7d9fd664a413b36c28c63d92f4390db9e203eee8
tomikorzu <tomykorzu@icloud.com>
2024-10-09 12:16:11 -0300

    feat(index.css / mainFunctions.js): delete innecesary code

--- src/utils/mainFunctions.js
+++ src/utils/mainFunctions.js
@@ -1,12 +1,5 @@
 const body = document.querySelector("body");
 
-export function redirectPage(url, className, time) {
-  body.classList.add(`${className}`);
-  setTimeout(() => {
-    window.location.href = url;
-  }, time);
-}
-
 export function userAlert(title, message) {
   const alertMenu = document.createElement("div");
   alertMenu.classList.add("alert-menu");
@@ -25,9 +18,4 @@ export function userAlert(title, message) {
       alertMenu.remove();
     }, 500);
   }, 2000);
-}
-
-export function applyBlur() {
-  const backDrop = document.querySelector(".back-drop");
-  backDrop.classList.toggle("blur");
 }
